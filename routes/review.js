5dfca00ff0a9951211afd5b818a73abf76b26a06
Ashish <ASHISH-VASAVA@users.noreply.github.com>
2025-07-07 23:00:34 +0530

    Clean project: added .gitignore, removed node_modules, reinstalled deps

---
+++ routes/review.js
@@ -0,0 +1,43 @@
+const express=require("express");
+const router=express.Router({mergeParams:true});
+const Listing = require("../models/listing.js"); 
+const wrapAsync=require("../utils/wrapAsync.js");
+const ExpressError=require("../utils/ExpressError.js")
+const {listingSchema,reviewSchema}=require("../schema.js");
+const Review=require("../models/review.js");
+
+const validateReview=(req,res,next) =>{
+    let {error}=reviewSchema.validate(req.body);
+    if(error){
+        let errMsg=error.details.map((el) =>el.message).join(",");
+       throw new ExpressError(400,errMsg);
+    }else{
+        next();
+    }
+};
+
+//Reviews Route
+// post route
+router.post("/",validateReview, wrapAsync ( async (req,res) =>{
+    let listing=await Listing.findById(req.params.id);
+    let newReview = new Review(req.body.review);
+
+    listing.reviews.push(newReview);
+
+    await newReview.save();
+    await listing.save();
+
+    res.redirect(`/listings/${listing._id}`);
+}));
+
+//Delete reviews route
+
+router.delete("/:reviewId", wrapAsync(async(req,res) =>{
+    let {id,reviewId}=req.params;
+    await Listing.findByIdAndUpdate(id,{$pull:{review: reviewId}});
+    await  Review.findByIdAndDelete(reviewId);
+
+    res.redirect(`/listings/${id}`);
+}))
+
+module.exports=router;
